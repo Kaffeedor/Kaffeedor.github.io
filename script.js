ca077e0810ba91e8e4945d81243f6ca98d4b9836
murphy <49571205+Kaffeedor@users.noreply.github.com>
2024-03-24 14:26:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-var score=0;
-function clicked() {
-  console.log("Clicked");
-  score++;
-  document.getElementById("scoreSpan").innerHTML = score;
-  localStorage.setItem('scoreSpan', score)
-}
-var load_in=0
-if(load_in == 0) {
-  load_in=1;
-  if(localStorage.getItem('scoreSpan') !== null) {
+var load_in = false;
+var score = 0;
+
+if (load_in == false) {
+  load_in = true;
+  if (localStorage.getItem('scoreSpan') !== null) {
     score = localStorage.getItem('scoreSpan');
     document.getElementById('scoreSpan').innerHTML = score;
   }
+}
+
+
+// code functions
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function zoomin(img_id, zoom_val){
+    var myImg = document.getElementById(img_id);
+    var currWidth = myImg.clientWidth;
+    myImg.style.width = (currWidth - zoom_val) + "px";
+}
+
+function zoomout(img_id), zoom_val{
+    var myImg = document.getElementById(img_id);
+    var currWidth = myImg.clientWidth;
+    myImg.style.width = (currWidth + zoom_val) + "px";
+}
+
+
+// website functions
+function clicked() {
+  score++;
+  document.getElementById("scoreSpan").innerHTML = score;
+  localStorage.setItem('scoreSpan', score);
+  zoomin("redbutton", 3);
+  sleep(50);
+  zoomout("redbutton", 3);
 }
